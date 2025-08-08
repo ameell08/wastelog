7e821d6a8dec682f2a0634c4f099250896be4235
ameell08 <amelia.safitri.wahyu@gmail.com>
2025-08-08 13:58:17 +0700

    export pdf dan excel superadmin

---
+++ resources/views/dashboard/export_limbahdiolah_pdf.blade.php
@@ -0,0 +1,172 @@
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <style>
+        body {
+            font-family: "Times New Roman", Times, serif;
+            margin: 6px 20px 5px 20px;
+            line-height: 15px;
+            position: relative;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        td,
+        th {
+            padding: 4px 3px;
+        }
+
+        th {
+            text-align: left;
+        }
+
+        .d-block {
+            display: block;
+        }
+
+        img.image {
+            width: auto;
+            height: 80px;
+            max-width: 150px;
+            max-height: 150px;
+        }
+
+        .text-right {
+            text-align: right;
+        }
+
+        .text-left {
+            text-align: left;
+        }
+
+        .text-center {
+            text-align: center;
+        }
+
+        .p-1 {
+            padding: 5px 1px 5px 1px;
+        }
+
+        .font-10 {
+            font-size: 10pt;
+        }
+
+        .font-11 {
+            font-size: 11pt;
+        }
+
+        .font-12 {
+            font-size: 12pt;
+        }
+
+        .font-13 {
+            font-size: 13pt;
+        }
+
+        .border-bottom-header {
+            border-bottom: 1px solid;
+        }
+
+        .border-all,
+        .border-all th,
+        .border-all td {
+            border: 1px solid;
+        }
+
+        .footer {
+            margin-top: 40px;
+        }
+
+        .ttd {
+            margin-top: 60px;
+        }
+
+        .footer-table {
+            width: 100%;
+            margin-top: 30px;
+        }
+
+        .footer-table td {
+            vertical-align: top;
+        }
+
+        .tanggal-cetak {
+            text-align: right;
+            font-size: 10pt;
+        }
+    </style>
+</head>
+
+<body>
+    <table class="border-bottom-header">
+        <tr>
+            <td width="15%" class="text-center">
+                <img src="{{ public_path('img/ptpria.png') }}" style="width: 100px;">
+            </td>
+            <td width="85%">
+                <span class="text-center d-block font-13 font-bold mb-1">PT PUTRA RESTU IBU ABADI</span>
+                <span class="text-center d-block font-11 font-bold mb-1">Pusat Pengangkutan, Pengolahan, Pemanfaatan dan
+                    Pengumpulan Limbah B3</span>
+                <span class="text-center d-block font-10">Jl. Raya Lakardowo Ds. Lakardowo, Kec. Jetis, Kab. Mojokerto
+                    Jawa Timur</span>
+                <span class="text-center d-block font-10">Telepon (0321)361212, Fax. (0321) 365322</span>
+                <span class="text-center d-block font-10">Laman: www.ptpria.co.id</span>
+            </td>
+        </tr>
+    </table>
+
+    <h3 class="text-center">DAFTAR LIMBAH DIOLAH</h3>
+
+    <table class="border-all">
+        <thead>
+            <tr>
+                <th class="text-center">No</th>
+                <th>Tanggal</th>
+                <th>Mesin</th>
+                <th>Kode Limbah</th>
+                <th>Berat Input (Kg)</th>
+                <th>Bottom Ash (2%) Kg </th>
+                <th>Fly Ash (0.4%) Kg </th>
+                <th>Flue Gas (1%) Kg </th>
+            </tr>
+        </thead>
+        <tbody>
+            @php $no = 1; @endphp
+            @foreach ($limbahDiolah as $o)
+                @foreach ($o->detailLimbahDiolah as $detail)
+                    <tr>
+                        <td class="text-center">{{ $no++ }}</td>
+                        <td>{{ \Carbon\Carbon::parse($o->created_at)->format('d/m/Y') }}</td>
+                        <td>{{ $o->mesin->no_mesin ?? '-' }}</td>
+                        <td>{{ $detail->kodeLimbah->kode ?? '-' }}</td>
+                        <td>{{ $detail->berat_kg }}</td>
+                        <td>{{ $detail->bottom_ash }}</td>
+                        <td>{{ $detail->fly_ash }}</td> 
+                        <td>{{ $detail->flue_gas }}</td>
+                    </tr>
+                @endforeach
+            @endforeach
+        </tbody>
+    </table>
+
+    <p class="tanggal-cetak">
+        Dicetak pada: {{ \Carbon\Carbon::now()->format('d/m/Y H:i') }}
+    </p>
+
+    <table class="footer-table">
+        <tr>
+            <td width="70%"></td>
+            <td class="text-center">
+                Mengetahui,<br>
+                Kepala Manajer<br><br><br><br><br>
+                (...........................................)
+            </td>
+        </tr>
+    </table>
+</body>
+
+</html>
